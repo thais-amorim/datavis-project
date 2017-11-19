8801946f3da6f3cd6a92261815ff515209ea2e59
Thais Marinho <thasmarinho@gmail.com>
2017-11-18 23:09:48 -0300

    Clean inflow and population data

--- js/scripts/map.js
+++ js/scripts/map.js
@@ -1,10 +1,84 @@
-let map = L.map('mapid').setView([11.3243433, 0.716431], 3);
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-  minZoom: 2,
-  maxZoom: 16})
-  .addTo(map);
-	
+let map = L.map('mapid', {
+                minZoom:2,
+                maxZoom:4
+                }).setView([25, 10], 2);
+let geojson;
+
+L.control.scale().addTo(map);
+
+function style(feature) {
+	/*if(activeBtn == 0)
+		*/return {
+			fillColor: '#FFFFFF',
+			weight: 2,
+			opacity: 1,
+			color: '#969E95',
+			dashArray: '3',
+			fillOpacity: 1.0
+		};/*
+	else if(activeBtn == 1)
+		data = softwareEngineerData;
+	else if(activeBtn == 2)
+		data = webDeveloperData;
+	else if(activeBtn == 3)
+		data = dataAnalystData;
+	else if(activeBtn == 4)
+		data = networkEngineerData;
+
+	var salary = data[feature.properties.name] ? parseFloat(data[feature.properties.name]) : -1;
+
+	return {
+		fillColor: getColor(salary),
+		weight: 2,
+		opacity: 1,
+		color: '#969E95',
+		dashArray: '3',
+		fillOpacity: 1.0
+	};*/
+}
+
+function highlightFeature(e) {
+	var layer = e.target;
+
+	layer.setStyle({
+		weight: 2,
+		color: '#7A8079',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera) {
+		layer.bringToFront();
+	}
+
+	info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+	geojson.resetStyle(e.target);
+	info.update();
+}
+
+function onEachFeature(feature, layer) {
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight
+	});
+
+	layer.on("click", function(e){
+		console.log(feature.properties.name); // botao que tu quiser
+		zoomToFeature(e);
+	});
+}
+
+function loadMap() {
+	geojson = L.geoJson(worldData, {
+		style: style,
+		onEachFeature: onEachFeature
+	}).addTo(map);
+}
+
+loadMap();
 let primaryType = ["Homicide","Robbery","Burglary"],
       colors 		= ["#EF793E", "#31659B", "#299F78"],
   	colorScale 	= d3.scale.ordinal().domain(primaryType).range(colors);
@@ -19,7 +93,7 @@ legend.onAdd = function (map) {
 			text 	= L.DomUtil.create('span');
 
 		circle.style.backgroundColor = colors[i];
-		text.textContent = primaryType[i];	
+		text.textContent = primaryType[i];
 		row.append(circle);
 		row.append(text);
 
@@ -34,7 +108,7 @@ legend.onAdd = function (map) {
    			.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
    			.join(' ');
       }
-	
+
   d3.csv("Crimes_Chicago_Sep2017.csv", function (data) {
       let dtgFormat 		= d3.time.format.utc("%m/%d/%Y %H:%M:%S"),
       	dateFormat		= d3.time.format("%m/%d/%Y"),
@@ -54,10 +128,10 @@ legend.onAdd = function (map) {
     	let typeDimension 		= facts.dimension(d => d.type),
     		timeDimension 		= facts.dimension(d => d.Date),
 			  		typeByDayDimension 	= facts.dimension(d => [d.type,d3.time.day(d.date)]);
-			  
+
 			  	let typeGroup 		= typeDimension.group(),
 			  		typeByDayGroup 	= typeByDayDimension.group();
-	
+
 			  	let typesOrdered = typeGroup.top(Infinity).map(d => d.key);
 
 let popupText;
