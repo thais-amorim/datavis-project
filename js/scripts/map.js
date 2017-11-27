576296dfa9450f6c41f111c9f26f8abd881a9ec0
Thais Marinho <thasmarinho@gmail.com>
2017-11-26 22:13:09 -0300

    Add inflow data

--- js/scripts/map.js
+++ js/scripts/map.js
@@ -1,155 +1,152 @@
-let map = L.map('mapid', {
-                minZoom:2,
-                maxZoom:4
-                }).setView([25, 10], 2);
+let populationByCountry = d3.map();
+let inflowByCountry = d3.map();
+
+let worldMap = L.map('mapid', {
+  minZoom: 2,
+  maxZoom: 4
+}).setView([25, 10], 2);
+
 let geojson;
 
-L.control.scale().addTo(map);
+L.control.scale().addTo(worldMap);
 
-function style(feature) {
-	/*if(activeBtn == 0)
-		*/return {
-			fillColor: '#FFFFFF',
-			weight: 2,
-			opacity: 1,
-			color: '#969E95',
-			dashArray: '3',
-			fillOpacity: 1.0
-		};/*
-	else if(activeBtn == 1)
-		data = softwareEngineerData;
-	else if(activeBtn == 2)
-		data = webDeveloperData;
-	else if(activeBtn == 3)
-		data = dataAnalystData;
-	else if(activeBtn == 4)
-		data = networkEngineerData;
+let info = L.control();
 
-	var salary = data[feature.properties.name] ? parseFloat(data[feature.properties.name]) : -1;
+info.onAdd = function(map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+};
 
-	return {
-		fillColor: getColor(salary),
-		weight: 2,
-		opacity: 1,
-		color: '#969E95',
-		dashArray: '3',
-		fillOpacity: 1.0
-	};*/
+info.update = function(props) {
+  if (activeBtn == 0)
+    data = {};
+  else if (activeBtn == 1)
+    data = {};
+  // data = inflowData;
+  else if (activeBtn == 2)
+    data = {};
+  // data = outflowData;
+
+  let inflowTax = props && inflowByCountry.get(props.name) ? inflowByCountry.get(props.name) : -1;
+
+  this._div.innerHTML = '<h3>Average flow</h3>' + (props ?
+    '<b>' + props.name + '</b><br />' + inflowTax + ' inflows' :
+    'Hover over a country');
+};
+
+info.addTo(worldMap);
+
+let domain = [0, 1000, 5000, 10000, 12000, 15000, 18000],
+  brewerColors = colorbrewer.OrRd[7];
+
+let quantize = d3.scale.quantile()
+  .domain(domain)
+  .range(brewerColors);
+
+function style(features) {
+  let colorToFill;
+
+  if (activeBtn == 0) {
+    colorToFill = '#FFFFFF';
+  } else if (activeBtn == 1) {
+    colorToFill = quantize(inflowByCountry.get(features.properties.name));
+  } else if (activeBtn == 2) {
+    // colorToFill = quantize(outflowByCountry.get(features.properties.name));
+    colorToFill = brewerColors[0];
+  }
+  return {
+    fillColor: colorToFill,
+    weight: 2,
+    opacity: 1,
+    color: '#969E95',
+    dashArray: '3',
+    fillOpacity: 0.8
+  };
 }
 
 function highlightFeature(e) {
-	var layer = e.target;
+  let layer = e.target;
 
-	layer.setStyle({
-		weight: 2,
-		color: '#7A8079',
-		dashArray: '',
-		fillOpacity: 0.7
-	});
+  layer.setStyle({
+    weight: 2,
+    opacity: 1,
+    color: '#969E95',
+    dashArray: '3',
+    fillOpacity: 0.8
+  });
 
-	if (!L.Browser.ie && !L.Browser.opera) {
-		layer.bringToFront();
-	}
+  if (!L.Browser.ie && !L.Browser.opera) {
+    layer.bringToFront();
+  }
 
-	info.update(layer.feature.properties);
+  info.update(layer.feature.properties);
 }
 
 function resetHighlight(e) {
-	geojson.resetStyle(e.target);
-	info.update();
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+function zoomToFeature(e) {
+  worldMap.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
-	layer.on({
-		mouseover: highlightFeature,
-		mouseout: resetHighlight
-	});
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight
+  });
 
-	layer.on("click", function(e){
-		console.log(feature.properties.name); // botao que tu quiser
-		zoomToFeature(e);
-	});
+  layer.on("click", function(e) {
+    zoomToFeature(e);
+  });
 }
 
 function loadMap() {
-	geojson = L.geoJson(worldData, {
-		style: style,
-		onEachFeature: onEachFeature
-	}).addTo(map);
+  geojson = L.geoJson(worldData, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(worldMap);
 }
 
-loadMap();
-let primaryType = ["Homicide","Robbery","Burglary"],
-      colors 		= ["#EF793E", "#31659B", "#299F78"],
-  	colorScale 	= d3.scale.ordinal().domain(primaryType).range(colors);
+d3.csv("data/worldPopulation.csv", function(data) {
+  data.forEach(p => populationByCountry.set(p.countryName, +p.population2015));
+});
 
-let legend = L.control({position: 'topright'});
-legend.onAdd = function (map) {
-  	let div = L.DomUtil.create('div', 'info legend');
+d3.csv("data/inflow_country.csv", function(data) {
+  data.forEach(function(d) {
+    let pop = populationByCountry.get(d.Country);
+    let tax = pop > 0 ? 100000 * d.inflow2015 / pop : 0;
 
-    for (i in primaryType) {
-    	let row 	= L.DomUtil.create('div','type'),
-			circle 	= L.DomUtil.create('div','circle'),
-			text 	= L.DomUtil.create('span');
-
-		circle.style.backgroundColor = colors[i];
-		text.textContent = primaryType[i];
-		row.append(circle);
-		row.append(text);
-
-        div.append(row);
-    }
-    return div;
-}
-//legend.addTo(map);
-
-      function toTitleCase(str){
-      	return str.split(' ')
-   			.map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
-   			.join(' ');
-      }
-
-  d3.csv("Crimes_Chicago_Sep2017.csv", function (data) {
-      let dtgFormat 		= d3.time.format.utc("%m/%d/%Y %H:%M:%S"),
-      	dateFormat		= d3.time.format("%m/%d/%Y"),
-      	timeFormat		= d3.time.format("%I:%M %p"),
-      	dayNameFormat	= d3.time.format("%A");
-
-    	data.forEach(function(d){
-      	d.latitude      = +d.Latitude;
-      	d.longitude     = +d.Longitude;
-      	d.date 			= dtgFormat.parse(d.Date.substr(0,19));
-	d.type			= toTitleCase(d["Primary Type"]);
-	d.location      = toTitleCase(d["Location Description"]);
-	d.description   = toTitleCase(d.Description);
-    	});
-    	let facts = crossfilter(data);
-
-    	let typeDimension 		= facts.dimension(d => d.type),
-    		timeDimension 		= facts.dimension(d => d.Date),
-			  		typeByDayDimension 	= facts.dimension(d => [d.type,d3.time.day(d.date)]);
-
-			  	let typeGroup 		= typeDimension.group(),
-			  		typeByDayGroup 	= typeByDayDimension.group();
-
-			  	let typesOrdered = typeGroup.top(Infinity).map(d => d.key);
-
-let popupText;
-
-  	data.forEach(function(d){
-      	let circle = L.circle([d.latitude, d.longitude], 100, {
-	color: colorScale(d.type),
-	weight: 2,
-	fillColor: colorScale(d.type),
-	fillOpacity: 0.5})
-	.addTo(map);
-
-	popupText = "Crime Type: "+d.type+"<br>"
-+"Date: "+dateFormat(d.date)+" ("+dayNameFormat(d.date)+")<br>"
-+"Time: "+timeFormat(d.date)+"<br>"
-+"Location: "+d.location+"<br>"
-+d.description+"<br>";
-
-      	circle.bindPopup(popupText);
-  	});
+    tax = Math.round(tax);
+    inflowByCountry.set(d.Country, tax);
   });
+
+  loadMap();
+});
+
+let legend = L.control({
+  position: 'bottomright'
+});
+
+legend.onAdd = function(map) {
+
+  let div = L.DomUtil.create('div', 'info legend'),
+    labels = [],
+    n = brewerColors.length,
+    from, to;
+
+  for (let i = 0; i < n; i++) {
+    let c = brewerColors[i];
+    let fromto = [domain[i], domain[i + 1]];
+    labels.push(
+      '<i style="background:' + brewerColors[i] + '"></i> ' +
+      d3.round(fromto[0]) + (d3.round(fromto[1]) ? '&ndash;' + d3.round(fromto[1]) : '+'));
+  }
+
+  div.innerHTML = labels.join('<br>');
+  return div;
+};
+
+legend.addTo(worldMap);
--- js/scripts/worldMapFunctional.js
+++ js/scripts/worldMapFunctional.js
@@ -1,1 +1,14 @@
 let activeBtn = 0;
+
+function updateToInflow() {
+  if(activeBtn != 1) {
+    activeBtn = 1;
+    loadMap();
+  }
+}
+function updateToOutflow() {
+  if(activeBtn != 2) {
+    activeBtn = 2;
+    loadMap();
+  }
+}
